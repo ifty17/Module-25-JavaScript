c04bacf90e9c6f2b5b127d3aee9f759a11ac3adb
ifty17 <iftyrahatkabir@gmail.com>
2022-08-15 15:44:27 +0600

    module 20 - 3

--- events.js
+++ events.js
@@ -1,6 +1,10 @@
-console.log("this is separate js file");
+// option 3 Directly set on html element 
+//<button onclick="console.log(10)">Click Me</button>;
 
-//Option 2: on click function
+
+//Option 2: on click function in HTML element
+//<button onclick="makeRed()">Make Red</button>;
+//IMPORTANT
 function makeRed() {
   document.body.style.backgroundColor = "red";
 }
@@ -19,3 +23,24 @@ console.log(makePurpleButton);
 makePurpleButton.onclick = function makePurple() {
   document.body.style.backgroundColor = "purple";
 }
+
+//option 4
+ const plinkButton = document.getElementById('make-pink');
+       plinkButton.addEventListener('click', makePink);
+       function makePink(){
+        document.body.style.backgroundColor = 'pink';
+       }
+
+//option 4 another 
+const makeGreenButton = document.getElementById("make-green");
+makeGreenButton.addEventListener("click", function () {
+  document.body.style.backgroundColor = "green";
+});
+
+//option 4 final
+//IMPORTANT
+document
+  .getElementById("make-goldenRod")
+  .addEventListener("click", function () {
+    document.body.style.backgroundColor = "goldenrod";
+  });
